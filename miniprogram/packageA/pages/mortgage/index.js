82d1c2272d67800de84ca60fd18d8c958ed547d6
mh <1274323054@qq.com>
2021-11-01 23:18:39 +0800

    1

--- miniprogram/packageA/pages/mortgage/index.js
+++ miniprogram/packageA/pages/mortgage/index.js
@@ -1,66 +1,57 @@
 // packageA/pages/mortgage/index.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    dataList: [
+      { month: 1, monthAmount: 7166.93, reAmount: 8166.66, reTotal: 21825.47 },
+      { month: 2, monthAmount: 7166.93, reAmount: 8166.66, reTotal: 20825.47 },
+      { month: 3, monthAmount: 7166.93, reAmount: 8166.66, reTotal: 18825.47 },
+      { month: 4, monthAmount: 7166.93, reAmount: 8166.66, reTotal: 21825.47 },
+      { month: 5, monthAmount: 7166.93, reAmount: 8166.66, reTotal: 21825.47 },
+      { month: 6, monthAmount: 7166.93, reAmount: 8166.66, reTotal: 21825.47 },
+      { month: 7, monthAmount: 7166.93, reAmount: 8166.66, reTotal: 21825.47 },
+    ],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
-  },
+  onLoad: function (options) {},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function () {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
-  },
+  onShow: function () {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function () {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function () {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function () {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function () {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  }
-})
+  onShareAppMessage: function () {},
+});
